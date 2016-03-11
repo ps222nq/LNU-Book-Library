4c3d0ca19b02dc677b35df7bb108d6a6814c7196
Pepyn Swagemakers <ps222nq@student.lnu.se>
2016-03-11 12:14:51 +0100

    API test for Delete Book

--- test/DeleteABook.api.spec.js
+++ test/DeleteABook.api.spec.js
@@ -1,24 +1,28 @@
-/*
+
 // API documentation - https://github.com/visionmedia/supertest
 var request = require('supertest');
 
 var app = require("../app");
 
 
-describe("Ping Resource", function () {
+describe("Delete a  book", function () {
 
-    describe("GET /api/ping", function () {
+    describe("DELETE /api/books/3", function () {
 
-        it("respond with pong", function (done) {
+        it("respond with 200", function (done) {
 
             request(app)
-                .get('/api/ping')
-                .set('Accept', 'application/json')
-                .expect(200, {
-                    answer: "pong"
-                },done);
+                .del('/api/books/3')
+                .expect(200, done());
+
+        });
+
+        it("respond with 404", function (done) {
+
+            request(app)
+                .del('/api/books/glenn')
+                .expect(404, done());
 
         });
     });
 });
-*/
