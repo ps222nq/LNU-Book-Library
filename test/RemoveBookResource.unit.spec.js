2191241642c4e593af668b61a2260e709aa4a195
Pepyn Swagemakers <ps222nq@student.lnu.se>
2016-03-12 14:26:23 +0100

    working on delete book unit tests

--- test/RemoveBookResource.unit.spec.js
+++ test/RemoveBookResource.unit.spec.js
@@ -1,18 +1,72 @@
-/*var expect    = require("chai").expect;
-var AddSimpleFunction = require("../app/AddSimpleFunction");
+var expect    = require("chai").expect;
+var fs = require("fs");
+var xml2js = require("xml2js");
+var LibraryDao = require("../app/dao/libraryDAO");
+var testXML = fs.readFile("./testbooks.xml", function(error, data) {
+    if (error) {
+        return error;
+    }
 
-describe("Simple addition", function () {
+    return data;
+});
 
-    describe("describe what the test do here...", function () {
+describe("RemoveBookResource", function() {
 
-        it("describe..", function () {
-            var a = 2;
-            var b = 4;
+    describe("test the deletion of books", function() {
 
-            var result = AddSimpleFunction.add(2,4);
+        it("test if reads from XML", function() {
 
-            expect(result).to.equal(6);
+            var result = LibraryDao.readXMLFile(function(error, data) {
+                if (error) {
+                    return console.log(error);
+                }
+
+                var parser  = new xml2js.Parser();
+                parser.parseString(data, function(err, res) {
+                    if (err) {
+                        console.log(err);
+                    }
+
+                    return res;
+                });
+                
+            });
+
+            expect(result).to.equal(testXML);
 
         });
+
+        //it("test if book has been deleted", function() {
+            // This should work by getting the array that is processed in RemoveBookResource
+            // And comparing its length to a mockup array where a book has been deleted
+            // But testing knowledge  and time allotted is insufficient at this time
+
+       // });
+
+        it("test if writing back to filesystem works", function() {
+
+            var working = true;
+            var foo = {
+                country : "Sweden",
+                capital: "Stockholm",
+                flagColors: ["blue", "yellow"],
+                weather: "cold"
+            };
+
+            var builder = new xml2js.Builder();
+            builder.buildObject(foo);
+
+            fs.writeFile("./dummyXMLFile.xml", xml, function(error){
+                if (error){
+                    working = false;
+                    return console.log(error)
+                }
+            });
+
+            expect(working).to.equal(true);
+
+
+         });
     });
-});*/
+    });
+});
