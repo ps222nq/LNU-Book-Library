2804e2dda81fd7c900c706a157624f82e0324a98
Pepyn Swagemakers <ps222nq@student.lnu.se>
2016-03-29 13:56:33 +0200

    adding some  more

--- test/EditBookResource.unit.spec.js
+++ test/EditBookResource.unit.spec.js
@@ -1,18 +1,73 @@
 /*var expect    = require("chai").expect;
-var AddSimpleFunction = require("../app/AddSimpleFunction");
+ var fs = require("fs");
+ var xml2js = require("xml2js");
+ var LibraryDao = require("../app/dao/libraryDAO");
+ var testXML = fs.readFile("./testbooks.xml", function(error, data) {
+ if (error) {
+ return error;
+ }
 
-describe("Simple addition", function () {
+ return data;
+ });
 
-    describe("describe what the test do here...", function () {
+ describe("EditBookResource", function() {
 
-        it("describe..", function () {
-            var a = 2;
-            var b = 4;
+ describe("test the editing of books", function() {
 
-            var result = AddSimpleFunction.add(2,4);
+ it("reads the XML file from file storage", function() {
 
-            expect(result).to.equal(6);
+ var result = LibraryDao.readXMLFile(function(error, data) {
+ if (error) {
+ return console.log(error);
+ }
 
-        });
-    });
-});*/
+ var parser  = new xml2js.Parser();
+ parser.parseString(data, function(err, res) {
+ if (err) {
+ console.log(err);
+ }
+
+ return res;
+ });
+
+ });
+
+ expect(result).to.equal(testXML);
+
+ });
+
+ //it("tests if book has been edited", function() {
+ // This should work by getting the array that is processed in EditBookResource
+ // And comparing the edited item to a mockup edited item at the index provided
+ // But testing knowledge  and time allotted is insufficient at this time
+
+ // });
+
+
+ it("writes back to the filesystem", function() {
+
+ var working = true;
+ var foo = {
+ country: "Sweden",
+ capital: "Stockholm",
+ flagColors: ["blue", "yellow"],
+ weather: "cold"
+ };
+
+ var builder = new xml2js.Builder();
+ builder.buildObject(foo);
+
+ fs.writeFile("./dummyXMLFile.xml", xml, function(error) {
+ if (error) {
+ working = false;
+ return console.log(error)
+ }
+ });
+
+ expect(working).to.equal(true);
+
+ });
+ });
+
+ });
+*/
--- test/GetBookResource.unit.spec.js
+++ test/GetBookResource.unit.spec.js
@@ -1,18 +1,77 @@
 /*var expect    = require("chai").expect;
-var AddSimpleFunction = require("../app/AddSimpleFunction");
+ var fs = require("fs");
+ var xml2js = require("xml2js");
+ var LibraryDao = require("../app/dao/libraryDAO");
+ var testXML = fs.readFile("./testbooks.xml", function(error, data) {
+ if (error) {
+ return error;
+ }
 
-describe("Simple addition", function () {
+ return data;
+ });
 
-    describe("describe what the test do here...", function () {
+ describe("RemoveBookResource", function() {
 
-        it("describe..", function () {
-            var a = 2;
-            var b = 4;
+ describe("test the deletion of books", function() {
 
-            var result = AddSimpleFunction.add(2,4);
+ it("reads the XML file from file storage", function() {
 
-            expect(result).to.equal(6);
+ var result = LibraryDao.readXMLFile(function(error, data) {
+ if (error) {
+ return console.log(error);
+ }
 
-        });
-    });
-});*/
+ var parser  = new xml2js.Parser();
+ parser.parseString(data, function(err, res) {
+ if (err) {
+ console.log(err);
+ }
+
+ return res;
+ });
+
+ });
+
+ expect(result).to.equal(testXML);
+
+ });
+
+ //it("tests if book has been deleted", function() {
+ // This should work by getting the array that is processed in RemoveBookResource
+ // And comparing its length to a mockup array where a book has been deleted
+ // But testing knowledge  and time allotted is insufficient at this time
+
+ // });
+
+ it("does not crash if list contains no books", function() {
+ // This should work by exercising RemoveBookResource and pulling out the array called bookArray
+ // And comparing it to an empty mockup array
+
+ });
+
+ it("writes back to the filesystem", function() {
+
+ var working = true;
+ var foo = {
+ country: "Sweden",
+ capital: "Stockholm",
+ flagColors: ["blue", "yellow"],
+ weather: "cold"
+ };
+
+ var builder = new xml2js.Builder();
+ builder.buildObject(foo);
+
+ fs.writeFile("./dummyXMLFile.xml", xml, function(error) {
+ if (error) {
+ working = false;
+ return console.log(error)
+ }
+ });
+
+ expect(working).to.equal(true);
+
+ });
+ });
+
+ });
